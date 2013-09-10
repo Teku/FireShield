2f33c09d21c4fb8d0d4e8c2db700b6a919f072cf
Joe Preston <josephwpreston@gmail.com>
2013-09-10 00:46:54 -0400

    Initial Commit

---
+++ src/main/co/anticraft/teku/sample/FireShield.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package co.anticraft.teku.sample;
+
+/**
+ * @author Teku
+ *
+ */
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+//import java.util.HashMap;
+import org.bukkit.entity.Player;
+//import org.bukkit.plugin.PluginDescriptionFile;
+//import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FireShield extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		// Code here...
+		getLogger().info("FireShield loaded.");
+	}
+	
+	@Override
+	public void onDisable(){
+		// Code here...
+		getLogger().info("FireShield unloaded.");
+	}
+	
+	// Command - /ignite
+	public boolean onCommand(CommandSender sender, Command cmd, String lavel, String[] args){
+		if(cmd.getName().equalsIgnoreCase("ignite")){ // If /ignite then...
+			if(!(sender instanceof Player)){
+				sender.sendMessage("This command can only be run by a player.");
+			}else{
+				Player player = (Player) sender;
+				// doSomething
+				player.setFireTicks(1000);
+				// Message
+				player.sendMessage("I don't know what you expected");
+			}
+			return true;
+		}
+		return false;
+	}
+}
